47c81be5017c1200de6d00cdcfb64147cd07106e
taufikcharlie <mohamadtaufik099@gmail.com>
2026-07-08 23:34:10 +0800

    chore: bootstrap auth-service Spring Boot skeleton

---
+++ auth-service/src/test/java/com/vibeus/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vibeus.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude="
+                + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+                + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
